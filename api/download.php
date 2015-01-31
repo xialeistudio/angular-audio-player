7ffb74c2757084b0997d93b45f77c40c0610e14b
xialeistudio <xialeistudio@gmail.com>
2015-01-31 18:42:39 +0800

    下载优化

--- api/download.php
+++ api/download.php
@@ -23,13 +23,17 @@ $params = array(
 $data = Request::get($url,$params);
 $data = json_decode($data,true);
 
+$name = $data['songinfo']['author'].' - '.$data['songinfo']['title'].'.mp3';
 foreach($data['bitrate'] as $row){
-	if($row['file_bitrate'] == 128){
+	if($row['file_bitrate'] == 192){
 		$url = $row['file_link'];
-		$tmp = Request::get($url);
+		//$tmp = Request::get($url);
 		header("Content-type: application/octet-stream"); //返回的文件
-		header("Content-Disposition: attachment; filename=download.mp3");//这里客户端的弹出对话框，对应的文件名
-		echo $tmp;
+		header("Content-Disposition: attachment; filename=".$name);//这里客户端的弹出对话框，对应的文件名
+		$fp = fopen($row['file_link'],'rb');
+		while(!feof($fp)){
+			echo fread($fp,4096);
+		}
 		exit;
 	}
 }
